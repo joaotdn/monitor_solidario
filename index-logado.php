f1280207995bdbb6d772faeaec1fd7c3addc0d94
Joao Teodoro <joaotdn@gmail.com>
2015-11-26 10:30:52 -0300

    projeto atualizado

---
+++ index-logado.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html class="no-js" lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monitor Solidário</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,800,700,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="fonts/foundation-icons.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+
+    <header class="top-bar bg-primary">
+      <div class="row">
+        <div class="small-12 columns">
+          <div class="top-bar-left">
+            <ul class="menu bg-primary">
+              <li><h1 class="font-bold no-margin left"><a href="#" class="white" title="Página principal"><i class="fi-torsos-all"></i></a></h1></li>
+              <li class="search-input"><input type="search" placeholder="O que você procura?"></li>
+            </ul>
+          </div>
+
+          <div class="top-bar-right">
+            <div class="divide-10"></div>
+            <ul class="menu bg-primary white">
+              <li>Olá João</li>
+              <li class="search-input"><button type="button" class="success button">Publique um vídeo!</button></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <div id="main-page" class="small-12 left">
+      <div class="divide-30"></div>
+      <div class="row">
+
+        <div id="last-posts" class="small-9 columns">
+          <header class="divide-20">
+            <p class="font-small"><time>Vídeo publicado em 12 de setembro de 2015 por <a href="#">joao teodoro</a></time></p>
+            <h4>Título do vídeo</h4>
+          </header>
+          <figure class="flex-video">
+            <iframe width="420" height="315" src="https://www.youtube.com/embed/MH6TJU0qWoY" frameborder="0" allowfullscreen></iframe>
+          </figure>
+          
+          <nav id="share-video" class="small-12 text-center left callout secondary">
+            <p class="font-small primary no-margin">Compartilhe esse vídeo</p>
+            <h1 class="no-margin">
+              <a href="#" class="left"><i class="fi-social-facebook"></i></a>
+              <a href="#" class="left"><i class="fi-social-twitter"></i></a>
+              <a href="#" class="left"><i class="fi-social-google-plus"></i></a>
+              <a href="#" class="left"><i class="fi-mail"></i></a>
+            </h1>
+          </nav>
+
+          <nav id="last-videos" class="small-12 left">
+            <ul class="vertical medium-horizontal menu">
+              <li>
+                <a href="#" class="d-iblock th">
+                  
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="d-iblock th">
+                  
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="d-iblock th">
+                  
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="d-iblock th">
+                  
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div id="user-bar" class="small-3 columns">
+          <ul class="no-bullet">
+            <li><a href="#"><span class="fi-home"></span> <span>Início</span></a></li>
+            <li><a href="#"><span class="fi-torso"></span> <span>Minha conta</span></a></li>
+            <li><a href="#"><span class="fi-video"></span> <span>Meus vídeos</span></a></li>
+          </ul>
+
+          <ul id="popular" class="no-bullet">
+            <li>
+              <h3 class="success">Mais vistos</h3>
+            </li>
+            <li>
+              <a href="#"><span class="fi-play"></span> Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+            </li>
+            <li>
+              <a href="#"><span class="fi-play"></span> Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+            </li>
+            <li>
+              <a href="#"><span class="fi-play"></span> Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+            </li>
+            <li>
+              <a href="#"><span class="fi-play"></span> Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+            </li>
+            <li>
+              <a href="#"><span class="fi-play"></span> Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+            </li>
+            <li>
+              <a href="#"><span class="fi-play"></span> Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+    
+
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/what-input/what-input.js"></script>
+    <script src="bower_components/foundation-sites/dist/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
